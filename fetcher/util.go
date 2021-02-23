5029e9a88a971f966ab4f1260a9bd9824a9571b3
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-02-23 20:02:34 +0100

    add fetcher util

--- fetcher/util.go
+++ fetcher/util.go
@@ -1,7 +1,16 @@
 package fetcher
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+)
 
 func stringIsEmpty(str string) bool {
 	return strings.TrimSpace(str) == ""
+}
+
+func getFieldTag(s interface{}, fieldName string, tagType string) string {
+	structField, _ := reflect.TypeOf(s).FieldByName(fieldName)
+
+	return string(structField.Tag.Get(tagType))
 }
