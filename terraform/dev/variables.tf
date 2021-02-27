6f4bba64a8c9e351dd390dd858041f7d38b53c76
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-02-27 18:21:26 +0100

    add default tf variables + google credentials

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -2,6 +2,14 @@ variable "project" {
   type = string
 }
 
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
 variable "services" {
   type = list(string)
   default = [
