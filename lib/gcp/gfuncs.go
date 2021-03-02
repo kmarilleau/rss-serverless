942f99da235ee08fffc49872fb63bcd21316f945
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-03-03 00:42:58 +0100

    add gfuncs

---
+++ lib/gcp/gfuncs.go
@@ -0,0 +1,21 @@
+package gfuncs
+
+import "os"
+
+func GetEnv() string {
+	switch os.Getenv("GOOGLE_CLOUD_PROJECT") {
+	case "PROD":
+		return "PROD"
+	default:
+		return "TEST"
+	}
+}
+
+func GetProjectID(env string) string {
+	switch env {
+	case "PROD":
+		return os.Getenv("GOOGLE_CLOUD_PROJECT")
+	default:
+		return "test-project"
+	}
+}
